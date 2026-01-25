5d207c93c72c456ecdd36ca7ead2ea4a4117cdcd
mieburungdara <rereitureza@gmail.com>
2026-01-25 23:08:10 +0800

    Fix syntax error: Renamed reserved keyword 'trait' to 't' in HeroProfileUI loop.

--- client/src/ui/HeroProfileUI.gd
+++ client/src/ui/HeroProfileUI.gd
@@ -6,35 +6,35 @@ extends Control
 @onready var close_btn = $Panel/VBoxContainer/CloseButton
 
 func _ready():
-	close_btn.pressed.connect(func(): hide())
-	ServerConnector.request_completed.connect(_on_request_completed)
+    close_btn.pressed.connect(func(): hide())
+    ServerConnector.request_completed.connect(_on_request_completed)
 
 func show_hero(hero_id: int):
-	show()
-	name_label.text = "Loading Hero..."
-	stats_label.text = ""
-	for child in traits_list.get_children():
-		child.queue_free()
-	ServerConnector.fetch_hero_profile(hero_id)
+    show()
+    name_label.text = "Loading Hero..."
+    stats_label.text = ""
+    for child in traits_list.get_children():
+        child.queue_free()
+    ServerConnector.fetch_hero_profile(hero_id)
 
 func _on_request_completed(endpoint, data):
-	if "hero/" in endpoint and endpoint.contains("/profile"):
-		_display_profile(data)
+    if "hero/" in endpoint and endpoint.contains("/profile"):
+        _display_profile(data)
 
 func _display_profile(profile):
-	name_label.text = profile.name
-	
-	# Stats
-	var stats_text = "COMBAT STATS:\n"
-	for key in profile.totalStats:
-		stats_text += "- %s: %d\n" % [key.to_upper(), profile.totalStats[key]]
-	stats_label.text = stats_text
-	
-	# Traits
-	for trait in profile.activeTraits:
-		var l = Label.new()
-		if trait is Dictionary:
-			l.text = "• %s (%s)" % [trait.name, trait.sourceSlot]
-		else:
-			l.text = "• %s (Hero)" % trait
-		traits_list.add_child(l)
+    name_label.text = profile.name
+    
+    # Stats
+    var stats_text = "COMBAT STATS:\n"
+    for key in profile.totalStats:
+        stats_text += "- %s: %d\n" % [key.to_upper(), profile.totalStats[key]]
+    stats_label.text = stats_text
+    
+    # Traits
+    for t in profile.activeTraits:
+        var l = Label.new()
+        if t is Dictionary:
+            l.text = "• %s (%s)" % [t.name, t.sourceSlot]
+        else:
+            l.text = "• %s (Hero)" % t
+        traits_list.add_child(l)
