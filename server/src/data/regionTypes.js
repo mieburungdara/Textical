f67f6b45fab008bcbe26d496c4e19b62b1b85bfe
mieburungdara <rereitureza@gmail.com>
2026-01-28 15:00:41 +0800

    docs: implement Region Visual Type Registry and server-side constants

---
+++ server/src/data/regionTypes.js
@@ -0,0 +1,33 @@
+﻿/**
+ * Region Visual Types Registry
+ * These match the scene types in the Godot client.
+ */
+const RegionTypes = {
+    TOWN: \"TOWN\",
+    FOREST: \"FOREST\",
+    MINE: \"MINE\",
+    DUNGEON: \"DUNGEON\",
+    RUINS: \"RUINS\",
+    VOLCANO: \"VOLCANO\",
+    DESERT: \"DESERT\",
+    SNOW: \"SNOW\",
+    SWAMP: \"SWAMP\",
+    GRAVEYARD: \"GRAVEYARD\",
+    OCEAN: \"OCEAN\",
+    STORM: \"STORM\",
+    AUTUMN: \"AUTUMN\",
+    CORAL: \"CORAL\",
+    ICE: \"ICE\",
+    LAVA: \"LAVA\",
+    FAIRY: \"FAIRY\",
+    ARENA: \"ARENA\",
+    CASTLE: \"CASTLE\",
+    SHIP: \"SHIP\",
+    PRISON: \"PRISON\",
+    GIANT: \"GIANT\",
+    HELL: \"HELL\",
+    GARDEN: \"GARDEN\",
+    WASTELAND: \"WASTELAND\"
+};
+
+module.exports = RegionTypes;
